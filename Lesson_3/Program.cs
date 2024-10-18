40cdb63ae50000ce02c3667a9f2aea7985955419
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-10-19 00:27:38 +0200

    test

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,80 @@
+﻿//int x = 10;
+
+//x += 2;
+
+//int y = 10;
+
+//y -= 2;
+//y = y - 2;
+
+//int z = 10;
+
+//z *= 2;
+
+//z = z * 2;
+
+//int a = 10;
+
+//a /= 2;
+//a = a / 2;
+
+//int B = 10;
+
+//int q = 10;
+
+//q = q + 1;
+//q += 1;
+//q++;
+//q--;
+
+//Console.WriteLine(x);
+
+
+int y = 3;
+int b = 2;
+int x = b * y++;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson_2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+byte _age = 42;
+int health = 100;
+uint score = 100;
+long ordersCount = 1000000000000000000;
+
+float speed = 100.5f;
+double kilimeters = 100.5;
+decimal price = 100.5m;
+
+
+const double PI = 3.14159265358;
+
+string name = "world";
+
+
+Console.WriteLine("Hello, World!");
